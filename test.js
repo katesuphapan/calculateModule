a1ff4c69656275755f544a51b468aede0e8703ea
Suphapan Boonngoen [TH] <SBoonngoen@kerrylogistics.com>
2019-07-25 12:05:55 +0700

    add divide

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const cal = require('./index');
 
 console.log(cal.plus(2, 4));
 console.log(cal.subtract(4, 1));
-console.log(cal.multiply(10,5));
+console.log(cal.multiply(10, 5));
+console.log(cal.divide(10,2));
+
 
 
